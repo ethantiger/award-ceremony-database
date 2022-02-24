070941b96db614d754a5f1e3f4e1404bd6fcd1f8
ethantiger <ethanwakefield007@gmail.com>
2022-02-23 23:41:13 -0500

    Removed styles and added boostrap CDN

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// components
+import Navbar from './components/Navbar'
+
+
+// styles
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
     </div>
   );
 }
